ea07c2d7a7440c9a062fa651e5d6ede367fee6cd
Petri Kataja <peejkoo@gmail.com>
2019-12-28 00:55:51 +0200

    6.18 Added redux-thunk and modified init function touse async action creator

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -5,13 +5,11 @@ import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 import { initAnecdotes } from './reducers/anecdoteReducer'
-import AnecdoteService from './services/anecdotes'
 
 const App = (props) => {
   useEffect(() => {
-    AnecdoteService
-      .getAll().then(anecdotes => props.initAnecdotes(anecdotes))
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    props.initAnecdotes()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
   return (
