1b08e3a1c99d1087394098bd5111183b27df3fc6
Petri Kataja <peejkoo@gmail.com>
2019-12-27 21:34:40 +0200

    6.8 Moved anecdote listing to own component

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,33 +1,13 @@
-import React from 'react';
-import { addVote } from './reducers/anecdoteReducer'
-import AnecdoteForm from './components/AnecdoteForm';
-
+import React from 'react'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id) => {
-    props.store.dispatch(addVote(id))
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort((a,b) => {
-        return a.votes === b.votes
-          ? 0 
-          : a.votes > b.votes ? -1 : 1
-      }).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
     </div>
   )
---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { addVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = (props) => {
+  const anecdotes = props.store.getState()
+
+  const vote = (id) => {
+    props.store.dispatch(addVote(id))
+  }
+
+  return (
+    <div>
+        {anecdotes.sort((a,b) => {
+          return a.votes === b.votes
+            ? 0 
+            : a.votes > b.votes ? -1 : 1
+        }).map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default AnecdoteList
