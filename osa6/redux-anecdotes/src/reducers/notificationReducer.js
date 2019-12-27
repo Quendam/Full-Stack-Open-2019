422c77ab8d3e19936f04d6582033259e08fc8c8b
Petri Kataja <peejkoo@gmail.com>
2019-12-27 22:00:05 +0200

    6.9 Added separate reducers for notification and anecdote list

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  message: 'Testing message'
+}
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  return state
+}
+
+export default reducer
